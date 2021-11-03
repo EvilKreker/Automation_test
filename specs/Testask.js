fd36d8a0d3e3b360d8a242f2d8fb69ada93d53c3
EvilKreker <t.melnik@pin-up.team>
2021-11-03 11:21:51 +0200

    Update variables names

--- specs/Testask.js
+++ specs/Testask.js
@@ -10,28 +10,41 @@ describe('TestTask', () => {
         await GoogleTableLoginPage.open();
 
 
-        const EmailLine = await $("#identifierId");
-        await EmailLine.setValue("moy.service666@gmail.com");
+        const EmailInput = await $("#identifierId");
+        await EmailInput.setValue("moy.service666@gmail.com");
 
-        const NextButtonMail = await $("#identifierNext > div > button > span");
-        await NextButtonMail.click();
+        const MailNextButton = await $("#identifierNext > div > button > span");
+        await MailNextButton.click();
 
         const RepeatTryButton = await $("//*[@id=\"next\"]/div/button/span");
         await RepeatTryButton.click();
 
-        await EmailLine.setValue("moy.service666@gmail.com");
+        await EmailInput.setValue("moy.service666@gmail.com");
 
-        await NextButtonMail.click();
+        await MailNextButton.click();
 
-        const Moreinfo = await $("#view_container > div > div > div.pwWryf.bxPAYd > div > div.WEQkZc > div > form > span > section > div > div > div > div:nth-child(1) > a");
-        await Moreinfo.click();
+        // const Moreinfo = await $("#view_c
+        // ontainer > div > div > div.pwWryf.bxPAYd > div > div.WEQkZc > div > form > span > section > div > div > div > div:nth-child(1) > a");
+        // const Moreinfo = await $("//*[@jsname='ngKiOe']");
+        // const Moreinfo = await $(".PrDSKc a");
+        // await Moreinfo.click();
+        // await browser.pause(5000)
+        // let moreInfoUrl = await browser.getUrl()
+        // await console.log(moreInfoUrl)
+        // await browser.pause(5000)
+        // await assert.deepStrictEqual(await moreInfoUrl, 'https://support.google.com/accounts/answer/7675428?hl=ru')
 
         await browser.url('https://support.google.com/accounts/answer/7675428?hl=ru');
 
+        // await browser.debug();
+
         const TitleLine = await $('/html[1]/body[1]/div[2]/div[1]/section[1]/div[1]/div[1]/article[1]/section[1]/h1[1]');
+        // const TitleLine = await $('.article .article-container h1');
+        // let TitleLine = await $('/descendant::h1');
+        // let TitleLine = await $("//*[@class='article-container']/h1");
 
         const value = await TitleLine.getText();
-        await assert.deepStrictEqual(value, 'Как войти в аккаунт в поддерживаемом браузере', 'YES');
+        await assert.deepStrictEqual(value, 'Как войти в аккаунт в поддерживаемом браузере');
 
         // await browser.debug()
     });
